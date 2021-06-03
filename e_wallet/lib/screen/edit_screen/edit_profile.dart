dcb59a1f93f63b1abb17a7570c1caf1a07171c60
Sinh <Bashintoba@gmail.com>
2021-06-03 16:33:27 +0700

    Add edit profile

--- e_wallet/lib/screen/edit_screen/edit_profile.dart
+++ e_wallet/lib/screen/edit_screen/edit_profile.dart
@@ -1,11 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class EditProfile extends StatefulWidget {
+  final String name;
+  EditProfile(this.name);
   @override
-  _EditProfileState createState() => _EditProfileState();
+  _EditProfileState createState() => _EditProfileState(name);
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final String name;
+  _EditProfileState(this.name);
+  final TextEditingController _nameInputCtrl = TextEditingController();
+  final TextEditingController _oldpassInputCtrl = TextEditingController();
+  final TextEditingController _newpassInputCtrl = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _nameInputCtrl.text = name;
+  }
+
+  Future<void> _alterDialogBuilder(String title,String error) async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Container(
+            child: Text(error),
+          ),
+          actions: [
+          FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Close Dialog"))
+        ],
+      );
+    });
+  }
+
+  void handleFormSubmit() async {
+    if (_oldpassInputCtrl.text.isNotEmpty && _newpassInputCtrl.text.isNotEmpty) {
+      User currentUser = FirebaseAuth.instance.currentUser;
+      UserCredential _authResult;
+      try {
+        _authResult = await currentUser.reauthenticateWithCredential(
+          EmailAuthProvider.credential(
+              email: currentUser.email,
+              password: _oldpassInputCtrl.text),
+        );
+      } catch(e){
+        _alterDialogBuilder("Error",e.toString());
+      }
+      if(_authResult != null && _authResult.user != null){
+        FirebaseAuth.instance.currentUser.updatePassword(_newpassInputCtrl.text).then((_) async {
+          DocumentReference docRef = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser.uid);
+          await docRef.update({
+            "name": _nameInputCtrl.text,
+          });
+          Navigator.pop(context);
+          _alterDialogBuilder("Notification","Change Info User Success");
+        }).catchError((e){
+          _alterDialogBuilder("Error",e.toString());
+        });
+      }
+    }
+    else if(_oldpassInputCtrl.text.isNotEmpty && _newpassInputCtrl.text.isEmpty){
+      _alterDialogBuilder("Error","New password was missing");
+    }
+    else if(_oldpassInputCtrl.text.isEmpty && _newpassInputCtrl.text.isNotEmpty){
+      _alterDialogBuilder("Error","Old password was missing");
+    }
+    else{
+      DocumentReference docRef = FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser.uid);
+      await docRef.update({
+        "name": _nameInputCtrl.text,
+      });
+      Navigator.pop(context);
+      _alterDialogBuilder("Notification","Change Info User Success");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,6 +102,16 @@ class _EditProfileState extends State<EditProfile> {
                 fontFamily: 'RobotoSlab',
                 fontWeight: FontWeight.w700),
           ),
+          actions: [
+            GestureDetector(
+                onTap: () {
+                  _alterDialogBuilder("Notification", "You don't need to write password if you don't need change");
+                },
+                child: Icon(
+                  Icons.help,
+                  color: Color(0xFFCC0047),
+                ))
+          ],
           backgroundColor: Colors.black,
           shadowColor: Colors.white,
         ),
@@ -46,8 +136,8 @@ class _EditProfileState extends State<EditProfile> {
                               size: 40,
                             ),
                           ),
-                          TextButton(
-                              onPressed: () {}, child: Text('Edit photo'))
+                          // TextButton(
+                          //     onPressed: () {}, child: Text('Edit photo'))
                         ],
                       ),
                       SizedBox(width: 40),
@@ -56,41 +146,28 @@ class _EditProfileState extends State<EditProfile> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              'First name',
+                              'Name',
                               style: TextStyle(
                                   color: Color(0xFF787878),
                                   fontFamily: 'RobotoSlab',
-                                  fontSize: 16,
+                                  fontSize: 28,
                                   fontWeight: FontWeight.w700),
                             ),
                             SizedBox(
                               height: 4,
                             ),
                             TextField(
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Divider(color: Color(0xFFCCCCCC), height: 14),
-                            Text(
-                              'Last name',
-                              style: TextStyle(
-                                  color: Color(0xFF787878),
-                                  fontFamily: 'RobotoSlab',
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            TextField(
-                              style: TextStyle(color: Colors.white),
+                              controller: _nameInputCtrl,
+                              style: TextStyle(fontSize: 30,color: Colors.white),
                             ),
                           ],
                         ),
                       )
                     ],
                   )),
-              SizedBox(height: 56),
+              SizedBox(height: 5),
               TextField(
+                controller: _oldpassInputCtrl,
                   style: TextStyle(color: Colors.white, fontSize: 18),
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.fromLTRB(0, 14, 0, 14),
@@ -99,11 +176,11 @@ class _EditProfileState extends State<EditProfile> {
                         fontSize: 20,
                         fontFamily: 'RobotoSlab',
                         fontWeight: FontWeight.w700),
-                    labelText: 'Email',
+                    labelText: 'Old Password',
                     filled: true,
                     fillColor: Color(0xFF1B1C1E),
                     prefixIcon: Icon(
-                      Icons.person,
+                      Icons.lock,
                       size: 26,
                       color: Color(0xFF8D8E90),
                     ),
@@ -115,6 +192,7 @@ class _EditProfileState extends State<EditProfile> {
                 height: 3,
               ),
               TextField(
+                controller: _newpassInputCtrl,
                   style: TextStyle(color: Colors.white, fontSize: 18),
                   decoration: InputDecoration(
                     contentPadding: EdgeInsets.fromLTRB(0, 14, 0, 14),
@@ -123,7 +201,7 @@ class _EditProfileState extends State<EditProfile> {
                         fontSize: 20,
                         fontFamily: 'RobotoSlab',
                         fontWeight: FontWeight.w700),
-                    labelText: 'Password',
+                    labelText: 'New Password',
                     filled: true,
                     fillColor: Color(0xFF1B1C1E),
                     prefixIcon: Icon(
@@ -156,7 +234,7 @@ class _EditProfileState extends State<EditProfile> {
                       fontSize: 18,
                       fontWeight: FontWeight.w500),
                 ),
-                onPressed: () {},
+                onPressed: () {handleFormSubmit();},
                 style: ButtonStyle(
                     backgroundColor:
                         MaterialStateProperty.all(Color(0xFF303030)),
--- e_wallet/lib/screen/profile.dart
+++ e_wallet/lib/screen/profile.dart
@@ -1,7 +1,9 @@
 import 'package:e_wallet/screen/edit_screen/edit_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:e_wallet/screen/signin_signup_screen/login_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:e_wallet/models/user_model.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -9,219 +11,189 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+
+  Future<void> _alterDialogBuilder(String title,String error) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Container(
+              child: Text(error),
+            ),
+            actions: [
+              FlatButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Close Dialog"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => EditProfile()));
-                },
-                child: Icon(
-                  Icons.edit,
+      child: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance.collection("users").doc(FirebaseAuth.instance.currentUser.uid).snapshots(),
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+          final UserModel user = UserModel.fromDocumentSnapshot(documentSnapshot: snapshot.data);
+            return Scaffold(
+              appBar: AppBar(
+                actions: [
+                  GestureDetector(
+                      onTap: () {
+                        // _alterDialogBuilder("Notification", "You don't need to write password if you don't need change");
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfile(user.name)));
+                      },
+                      child: Icon(
+                        Icons.edit,
+                        color: Color(0xFFCC0047),
+                      ))
+                ],
+                leading: BackButton(
                   color: Color(0xFFCC0047),
-                ))
-          ],
-          leading: BackButton(
-            color: Color(0xFFCC0047),
-          ),
-          title: Text(
-            'Profile',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 34,
-                fontFamily: 'RobotoSlab',
-                fontWeight: FontWeight.w700),
-          ),
-          backgroundColor: Colors.black,
-          shadowColor: Colors.white,
-        ),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(color: Colors.black),
-          child: SingleChildScrollView(
-            child: Column(children: [
-              Container(
-                  padding: EdgeInsets.fromLTRB(14, 14, 0, 14),
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 40,
-                            child: Icon(
-                              Icons.person,
-                              size: 40,
-                            ),
-                          ),
-
-                        ],
-                      ),
-                      SizedBox(width: 40),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                title: Text(
+                  'Profile',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 34,
+                      fontFamily: 'RobotoSlab',
+                      fontWeight: FontWeight.w700),
+                ),
+                backgroundColor: Colors.black,
+                shadowColor: Colors.white,
+              ),
+              body: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(color: Colors.black),
+                child: SingleChildScrollView(
+                  child: Column(children: [
+                    Container(
+                        padding: EdgeInsets.fromLTRB(14, 14, 0, 14),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
+                        child: Row(
                           children: [
-                            Text(
-                              'First name',
-                              style: TextStyle(
-                                  color: Color(0xFF787878),
-                                  fontFamily: 'RobotoSlab',
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700),
+                            Column(
+                              children: [
+                                CircleAvatar(
+                                  radius: 40,
+                                  child: Icon(
+                                    Icons.person,
+                                    size: 40,
+                                  ),
+                                ),
+
+                              ],
                             ),
-                            SizedBox(
-                              height: 4,
+                            SizedBox(width: 40),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'Name',
+                                    style: TextStyle(
+                                        color: Color(0xFF787878),
+                                        fontFamily: 'RobotoSlab',
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                  SizedBox(
+                                    height: 4,
+                                  ),
+                                  Text(
+                                    "${user.name}",
+                                    style: TextStyle(
+                                        color: Color(0xFFE6E6E6),
+                                        fontFamily: 'RobotoSlab',
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ],
+                              ),
                             ),
-                            Text(
-                              'Pháp',
-                              style: TextStyle(
-                                  color: Color(0xFFE6E6E6),
-                                  fontFamily: 'RobotoSlab',
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                            Divider(color: Color(0xFFCCCCCC), height: 14),
-                            Text(
-                              'Last name',
-                              style: TextStyle(
-                                  color: Color(0xFF787878),
-                                  fontFamily: 'RobotoSlab',
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text(
-                              'Huỳnh',
+                          ],
+                        )),
+                    SizedBox(height: 5),
+                    Container(
+                      padding: EdgeInsets.all(14),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Email',
                               style: TextStyle(
                                   color: Color(0xFFCCCCCC),
                                   fontFamily: 'RobotoSlab',
                                   fontSize: 18,
-                                  fontWeight: FontWeight.w700),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  )),
-              SizedBox(height: 56),
-              Container(
-                padding: EdgeInsets.all(14),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Email',
-                        style: TextStyle(
-                            color: Color(0xFFCCCCCC),
-                            fontFamily: 'RobotoSlab',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                          Text(
+                            "${user.email}",
+                            style: TextStyle(
+                                color: Color(0xFF787878),
+                                fontFamily: 'RobotoSlab',
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ],
                       ),
-                    ),
-                    Text(
-                      'longhappy@gmail.com',
-                      style: TextStyle(
-                          color: Color(0xFF787878),
-                          fontFamily: 'RobotoSlab',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Icon(
-                        Icons.arrow_forward_ios,
-                        color: Color(0xFF787878),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 3,
-              ),
-              Container(
-                padding: EdgeInsets.all(14),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        'Password',
-                        style: TextStyle(
-                            color: Color(0xFFCCCCCC),
-                            fontFamily: 'RobotoSlab',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
-                      ),
-                    ),
-                    Text(
-                      '*************',
-                      style: TextStyle(
-                          color: Color(0xFF787878),
-                          fontFamily: 'RobotoSlab',
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700),
-                    ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Icon(
-                        Icons.arrow_forward_ios,
-                        color: Color(0xFF787878),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 56,
-              ),
-              Container(
-                padding: EdgeInsets.all(14),
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.logout,
-                      color: Color(0xFFF40057),
-                      size: 34,
                     ),
                     SizedBox(
-                      width: 14,
+                      height: 56,
                     ),
-                    GestureDetector(
-                      onTap: () {
-                        FirebaseAuth.instance.signOut();
-                        Navigator.pop(context);
-                      },
-                      child: Text(
-                        'Logout',
-                        style: TextStyle(
+                    Container(
+                      padding: EdgeInsets.all(14),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(color: Color(0xFF1B1C1E)),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.logout,
                             color: Color(0xFFF40057),
-                            fontFamily: 'RobotoSlab',
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500),
+                            size: 34,
+                          ),
+                          SizedBox(
+                            width: 14,
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              FirebaseAuth.instance.signOut();
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Logout',
+                              style: TextStyle(
+                                  color: Color(0xFFF40057),
+                                  fontFamily: 'RobotoSlab',
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
+                    ),
+                  ]),
                 ),
               ),
-            ]),
-          ),
-        ),
-      ),
+            );
+        }
+      )
     );
   }
 }
